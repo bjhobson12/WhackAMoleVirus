7238dafdc4d5e4d03ac70df0abc88e35ff1a3380
bjhobson12 <bjhobson12@gmail.com>
2021-03-17 14:50:55 -0400

    My new branch

--- main.py
+++ main.py
@@ -12,28 +12,3 @@ y = 300
 
 xchange = 0
 ychange = 0
-
-game_over=False
-while not game_over:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            game_over=True
-        if event.type==pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                xchange = -20
-                ychange = 0
-            elif event.key == pygame.K_RIGHT:
-                xchange = 20
-                ychange = 0
-            elif event.key == pygame.K_UP:
-                xchange = 0
-                ychange = -20
-            elif event.key == pygame.K_DOWN:
-                xchange = 0
-                ychange = 20
-    x += xchange
-    y += ychange
-    pygame.draw.rect(dis,blue,[310,230,10,10])
-    pygame.display.update()
-pygame.quit()
-quit()
