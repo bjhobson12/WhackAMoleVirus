005a4f207d3b6118ef226594f1b41675c33280b1
RockoPoints <43047277+RockyManrique@users.noreply.github.com>
2021-03-25 11:33:53 -0400

    base game started

--- main.py
+++ main.py
@@ -56,45 +56,74 @@ if __name__ == '__main__':
     Popen(['./main'], shell=True)
     #Popen(['./main'], stderr=PIPE, stdout=PIPE, shell=True)
     # 
-
-    print("Exited python script")
-    sys.exit() 
+#------------ Start of Pygame code
+    
     import pygame
-    pygame.init()
-    dis=pygame.display.set_mode((640,480))
+   
 
+class Mole(pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = pygame.image.load("mole.gif").convert_alpha()
+        self.rect - self.image.get_rect()
+
+    #if hit, move mole
+    def flee(self):
+    x = random.randint(0, scrWidth-1-self.rect.width)
+    y = random.randint(0, scrHeight-1-self.rect.height)
+    self.rect.topleft = (x,y)
+
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+class Hammer(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("hammer.gif").convert_alpha()
+        self.rect = self.image.get_rect()
+
+    # check if shovel hit mole
+    def hit(self, target):
+        return self.rect.colliderect(target)
+
+    # follows the mouse cursor
+    def update(self, pt):
+        self.rect.center = pt
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+#------------Start of Main
+
+pygame.init()
+dis=pygame.display.set_mode((640,480))
+screen.fill(WHITE)
+pygame.display.update()
+pygame.display.set_caption('Whack-a-Mole Game')
+
+#Set height and width
+scrWidth, scrHeight = screen.get_size()
+
+#create sprites
+mole = Mole()
+shovel = Shovel()
+
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == MOUSEMOTION:
+            mousePos = pygame.mouse.get_pos()
+        if event.type == MOUSEBUTTONDOWN:
+            isPressed = True
+            
+  
+    
     pygame.display.update()
-    pygame.display.set_caption('Polymorphic Virus Gamification')
+pygame.quit()
+quit()
 
-    blue = (0,0,255)
-
-    x = 300
-    y = 300
-
-    xchange = 0
-    ychange = 0
-
-    game_over=False
-    while not game_over:
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                game_over=True
-            if event.type==pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    xchange = -20
-                    ychange = 0
-                elif event.key == pygame.K_RIGHT:
-                    xchange = 20
-                    ychange = 0
-                elif event.key == pygame.K_UP:
-                    xchange = 0
-                    ychange = -20
-                elif event.key == pygame.K_DOWN:
-                    xchange = 0
-                    ychange = 20
-        x += xchange
-        y += ychange
-        pygame.draw.rect(dis,blue,[310,230,10,10])
-        pygame.display.update()
-    pygame.quit()
-    quit()
+print("Exited python script")
+sys.exit() 
